1345c900c7f253f1746672e7701cae4c723591fa
Ethea2 <nathandres2001@gmail.com>
2023-09-02 21:52:38 +0800

    added: fixed the frontend folder being a submodule.

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,12 @@
+require('dotenv').config()
 const express = require('express')
 
 app = express()
 
 app.use('/api/hello', (req, res) => {
     res.send("Hello!")
+})
+
+app.listen(process.env.PORT, () => {
+    console.log('listening to port ' + process.env.PORT)
 })
