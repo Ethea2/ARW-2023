fc5029ea1369b76d9f624ac9518914bfc6fde50a
Ethea2 <nathandres2001@gmail.com>
2023-09-02 22:05:56 +0800

    added: -FIXED FRONTEND BEING A SUBMODULE.. PLZ WORK

---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="w-full h-screen flex items-center justify-center bg-black">
+      <div className='text-5xl text-emerald-300'>HELLO</div>
+    </div>
+  );
+}
+
+export default App;
