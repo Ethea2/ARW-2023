63f19df37eb7081cf8c21ea1f7825d2b6ea749e9
Ethea2 <nathandres2001@gmail.com>
2023-09-04 19:29:44 +0800

    added: -org selector having 2 columns, one for clust and one for orgs.

--- frontend/src/pages/organization_select/OrgSelect.jsx
+++ frontend/src/pages/organization_select/OrgSelect.jsx
@@ -5,19 +5,26 @@ import OrgShow from "./components/OrgShow"
 
 const OrgSelect = () => {
     const { data: orgs, loading, error } = useFetch("http://localhost:4000/api/orgs/")
+    const { data: clusters } = useFetch("http://localhost:4000/api/clusters/")
     const [orgID, setOrgID] = useState(null)
+    const [orgCluster, setOrgCluster] = useState(null)
 
     const handleOrgSelect = (event, id) => { 
         event.preventDefault()
         setOrgID(id)
     }
 
+    const handleClusterSelect = (event, cluster) => {
+        event.preventDefault()
+        setOrgCluster(orgs.filter(org => org.cluster === cluster))
+    }
+
     return (
-        <div className="grid grid-flow-row w-full h-screen font-bit">
+        <div className="grid grid-flow-row w-full h-screen font-bit"  style={{gridTemplateRows: '1fr 1fr'}}>
             {loading && <div>Loading...</div>}
             {error && <div>{error}</div>}
             {orgs && <OrgShow orgID={orgID} />}
-            {orgs && <OrgSelector orgs={orgs} handleOrgSelect={handleOrgSelect} />}
+            {orgs && <OrgSelector orgs={orgCluster} clusters={clusters} handleOrgSelect={handleOrgSelect} handleClusterSelect={handleClusterSelect}/>}
         </div>
     )
 }
--- frontend/src/pages/organization_select/components/OrgSelector.jsx
+++ frontend/src/pages/organization_select/components/OrgSelector.jsx
@@ -1,15 +1,28 @@
-const OrgSelector = ({ orgs, handleOrgSelect }) => {
+const OrgSelector = ({ orgs, clusters, handleOrgSelect, handleClusterSelect }) => {
     return (
-        <div>
+        <div className="p-2 h-full">
             <div className="w-full flex justify-center items-center">
                 <h1 className="text-3xl font-bold">Select your organization</h1>
             </div>
             <div className="grid grid-cols-2">
-                {orgs.map(org => (
-                    <button className="btn btn-primary m-2" key={org.id} onClick={(e) => handleOrgSelect(e, org.abbrv_name)}>
-                        {org.comp_name}
-                    </button>
-                ))}
+                <div className="flex flex-col">
+                    {
+                        clusters?.map(cluster => (
+                            <button className="btn btn-primary m-2" key={cluster._id} onClick={(e) => handleClusterSelect(e, cluster.name)}>
+                                {cluster.name}
+                            </button>
+                        ))
+                    }
+                </div>
+                <div className="flex flex-col">
+                    {
+                        orgs?.map(org => (
+                            <button className="btn btn-primary m-2" key={org._id} onClick={(e) => handleOrgSelect(e, org.abbrv_name)}>
+                                {org.abbrv_name}
+                            </button>
+                        ))
+                    }
+                </div>
             </div>
         </div>
     )
