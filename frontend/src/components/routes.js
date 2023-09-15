104d031a7323a83b750fe94ad7672959b0843f9c
Nathan Andres <nathandres2001@gmail.com>
2023-09-15 19:08:38 +0800

    added: temporary error page

--- frontend/src/components/routes.js
+++ frontend/src/components/routes.js
@@ -1,5 +1,6 @@
 import Countdown from '../pages/countdown/Countdown'
 import CrownQuest from '../pages/crownquest/CrownQuest'
+import ErrorPage from '../pages/errorpage/ErrorPage'
 import Homepage from '../pages/homepage/Homepage'
 import OrgSelect from '../pages/organization_select/OrgSelect'
 
@@ -8,7 +9,8 @@ const routes = [
     {path: '/homepage', element: <Homepage />},
     {path: '/', element: <Countdown />},
     {path: '/orgSelect', element: <OrgSelect />},
-    {path: '/crownquest', element: <CrownQuest />}
+    {path: '/crownquest', element: <CrownQuest />},
+    {path: '*', element: <ErrorPage />}
 ]
 
 export default routes
