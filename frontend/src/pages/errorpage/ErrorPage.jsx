aeb45eae0a57b35f182a44f5a13618671bfc44ad
Nathan Andres <nathandres2001@gmail.com>
2023-09-19 19:42:56 +0800

    added: background gradient for error page

--- frontend/src/pages/errorpage/ErrorPage.jsx
+++ frontend/src/pages/errorpage/ErrorPage.jsx
@@ -2,14 +2,12 @@ import logo from "../../assets/bright-colored-logo.png"
 
 const ErrorPage = () => {
     return (
-        <div className="w-full h-screen overflow-hidden relative flex flex-col justify-center items-center blur-sm md:gap-10"
+        <div className="w-full h-screen overflow-hidden relative flex flex-col justify-center items-center md:gap-10"
             style={{
-                backgroundImage: `url(/countdown-bg.jpg)`,
-                backgroundPosition: "bottom",
-                backgroundSize: "cover",
+                background: "linear-gradient(to bottom, #451F4D, #8E5FD5)",
             }}>
             <img src={logo} className="z-10 w-1/2 drop-shadow-glow_green" />
-            <div className="drop-shadow-glow_green text-emerald-400 text-4xl md:text-6xl font-bold">404 Page does not exist!</div>
+            <div className="drop-shadow-glow_green text-emerald-400 text-2xl md:text-5xl lg:text-7xl font-bold">404 Page does not exist!</div>
         </div>
     )
 }
