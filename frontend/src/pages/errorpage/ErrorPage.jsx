715f8d0d23522ce85923cc44454b696168109594
Nathan Andres <nathandres2001@gmail.com>
2023-09-19 19:34:57 +0800

    added: bg for non-existent routes

--- frontend/src/pages/errorpage/ErrorPage.jsx
+++ frontend/src/pages/errorpage/ErrorPage.jsx
@@ -2,7 +2,12 @@ import logo from "../../assets/bright-colored-logo.png"
 
 const ErrorPage = () => {
     return (
-        <div className="w-full h-screen overflow-hidden relative flex flex-col justify-center items-center md:gap-10">
+        <div className="w-full h-screen overflow-hidden relative flex flex-col justify-center items-center md:gap-10"
+            style={{
+                backgroundImage: `url(/countdown-bg.jpg)`,
+                backgroundPosition: "bottom",
+                backgroundSize: "cover",
+            }}>
             <img src={logo} className="z-10 w-1/2 drop-shadow-glow_green" />
             <div className="drop-shadow-glow_green text-emerald-400 text-4xl md:text-6xl font-bold">404 Page does not exist!</div>
         </div>
