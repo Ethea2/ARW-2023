42aa53ed9e67585125752193701d6114364a9c8d
Nathan Andres <nathandres2001@gmail.com>
2023-09-19 17:38:25 +0800

    added: removed animation for testing

--- frontend/src/pages/countdown/components/Timer.jsx
+++ frontend/src/pages/countdown/components/Timer.jsx
@@ -1,33 +1,28 @@
 import { motion, useMotionValue, useTransform } from "framer-motion";
 
 const Timer = ({ date, sevenDaysLeft }) => {
-    const x = useMotionValue(0);
-    const y = useMotionValue(0);
+    // const x = useMotionValue(0);
+    // const y = useMotionValue(0);
 
-    const rotateX = useTransform(y, [0, window.innerHeight], [-10, 10]);
-    const rotateY = useTransform(x, [0, window.innerWidth], [-10, 10]);
-    const scale = useTransform(x, [0, window.innerWidth], [0.95, 1.05]);
+    // const rotateX = useTransform(y, [0, window.innerHeight], [-10, 10]);
+    // const rotateY = useTransform(x, [0, window.innerWidth], [-10, 10]);
+    // const scale = useTransform(x, [0, window.innerWidth], [0.95, 1.05]);
 
-    function handleMouse(event) {
-        x.set(event.clientX);
-        y.set(event.clientY);
-    }
+    // function handleMouse(event) {
+    //     x.set(event.clientX);
+    //     y.set(event.clientY);
+    // }
 
     return (
-        <motion.div className="w-full h-screen m-auto flex justify-center items-center"
-            onMouseMove={handleMouse}
-            style={{
-                x: rotateX,
-                y: rotateY,
-                scale: scale,
-            }}>
+        <div className="w-full h-screen m-auto flex justify-center items-center"
+            >
             <div className={`flex flex-col gap-20 justify-center items-center w-9/12 ${sevenDaysLeft ? 'drop-shadow-glow_red text-red-700' : 'drop-shadow-glow_green text-emerald-400'}`}>
                 <div id="timer-container" className="flex flex-col md:flex-row md:flex justify-center items-center font-bit auto-cols-max w-full h-full">
                     {
                         Object.keys(date).map((key, index) => {
                             return (
-                                <div key={index} className="flex flex-col w-1/2 lg:w-fit lg:mx-10">
-                                    <span className="countdown  text-7xl md:text-8xl lg:text-9xl">
+                                <div key={index} className="flex flex-row md:flex-col w-1/2 lg:w-fit lg:mx-10">
+                                    <span className="countdown text-4xl md:text-5xl lg:text-9xl">
                                         <span style={{ "--value": date[key] }}></span>
                                     </span>
                                     <p className="z-10 mb-4 md:mb-0">{key.toUpperCase()}</p>
@@ -37,9 +32,9 @@ const Timer = ({ date, sevenDaysLeft }) => {
                     }
                 </div>
 
-                <p className="font-bit text-2xl">UNTIL ANNUAL RECRUITMENT WEEK 2023</p>
+                <p className="font-bit text-md md:text-2xl">UNTIL ANNUAL RECRUITMENT WEEK 2023</p>
             </div>
-        </motion.div>
+        </div>
     )
 }
 
