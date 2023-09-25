b56f16c509cc445d07717087664e0d3566917823
Nathan Andres <nathandres2001@gmail.com>
2023-09-25 19:22:32 +0800

    added: routes

--- frontend/src/pages/individual_org/IndivOrgPage.jsx
+++ frontend/src/pages/individual_org/IndivOrgPage.jsx
@@ -1,6 +1,10 @@
+import { useParams } from "react-router-dom"
+import useFetch from "../../hooks/useFetch"
 
 const IndivOrgPage = () => {
-    
+    const {data: org} = useFetch('')
+    const { orgId } = useParams()
+    console.log(orgId)
     return(
         <div className="w-full h-screen flex justify-center items-center">
             <span className="font-normal font-bit text-7xl">Indiv Org Test</span>
