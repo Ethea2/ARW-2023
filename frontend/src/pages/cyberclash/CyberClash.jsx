5d06592a38c813af1109f7a63a501b8ff8366adb
Nathan Andres <nathandres2001@gmail.com>
2023-09-26 17:33:33 +0800

    added: -loop guide

--- frontend/src/pages/cyberclash/CyberClash.jsx
+++ frontend/src/pages/cyberclash/CyberClash.jsx
@@ -3,7 +3,7 @@ import useFetch from "../../hooks/useFetch"
 import GameContainer from "./components/GameContainer"
 
 const CyberClash = () => {
-  const { data: cyberClash } = useFetch('/api/clash')
+  const { data: cyberClash,  loading} = useFetch('/api/clash')
 
   //GUIDE FOR DATA STRUCTURE
   useEffect(() => {
@@ -16,7 +16,8 @@ const CyberClash = () => {
 
   return (
     <div className="flex justify-center items-center w-full h-screen">
-      <div id="container" className="flex justify-center items-center border-2 border-accent w-64 h-64">
+      <div id="container" className="flex justify-center flex-col items-center border-2 border-accent w-64 h-64">
+        <GameContainer />
         <GameContainer />
       </div>
     </div>
