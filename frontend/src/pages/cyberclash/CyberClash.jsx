cff4c96cdf02b7e70d12914306f760909e68b1fa
Nathan Andres <nathandres2001@gmail.com>
2023-09-26 13:48:18 +0800

    added: -general data structure guide -print for data

--- frontend/src/pages/cyberclash/CyberClash.jsx
+++ frontend/src/pages/cyberclash/CyberClash.jsx
@@ -4,6 +4,15 @@ import useFetch from "../../hooks/useFetch"
 const CyberClash = () => {
   const { data: cyberClash } = useFetch('/api/clash')
 
+  //GUIDE FOR DATA STRUCTURE
+  useEffect(() => {
+    cyberClash?.map((game) => {
+      game.teams.map((vs) => {
+        console.log(`GAME: ${game.game} || ${vs[0].org} vs. ${vs[1].org}`)
+      })
+    } )
+  }, [cyberClash])
+
   return (
     <div className="flex justify-center items-center w-full h-screen">
       Hello
