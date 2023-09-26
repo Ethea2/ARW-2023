ff030724257326a207a4541e191501276758877e
Nathan Andres <nathandres2001@gmail.com>
2023-09-26 13:39:53 +0800

    added: setup data fetch

--- frontend/src/pages/cyberclash/CyberClash.jsx
+++ frontend/src/pages/cyberclash/CyberClash.jsx
@@ -1,7 +1,12 @@
+import { useEffect } from "react"
+import useFetch from "../../hooks/useFetch"
+
 const CyberClash = () => {
+  const { data: cyberClash } = useFetch('/api/clash')
+
   return (
     <div className="flex justify-center items-center w-full h-screen">
-        Hello
+      Hello
     </div>
   )
 }
