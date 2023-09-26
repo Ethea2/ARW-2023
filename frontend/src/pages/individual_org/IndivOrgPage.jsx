ebf318e1509b1fde3d77396cc3bb08b9dd5009ab
John Kovie Nino <thekovie@gmail.com>
2023-09-26 14:14:50 +0800

    fix: change screen height

--- frontend/src/pages/individual_org/IndivOrgPage.jsx
+++ frontend/src/pages/individual_org/IndivOrgPage.jsx
@@ -1,12 +1,13 @@
 import { useParams } from "react-router-dom"
 import useFetch from "../../hooks/useFetch"
+import BottomPageOrg from "./components/BottomPageOrg"
 
 const IndivOrgPage = () => {
     const {data: org} = useFetch('')
     const { orgId } = useParams()
     console.log(orgId)
     return(
-        <div className="w-full h-screen flex justify-center items-center">
+        <div className="w-full min-h-screen flex flex-col justify-center items-center">
             <span className="font-normal font-bit text-7xl">Indiv Org Test</span>
         </div>
     )
