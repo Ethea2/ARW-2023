1f8db697931365cbc892c771a5eec90184691ec8
kyoseia <87558896+kyoseia@users.noreply.github.com>
2023-09-27 02:05:53 +0800

    added: aboutus initial page todo: Error in background cannot be detected outsite public folder, cannot import SVG icon as the button

--- frontend/src/pages/aboutus/AboutUs.jsx
+++ frontend/src/pages/aboutus/AboutUs.jsx
@@ -1,40 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Drawer from '../../components/drawer/Drawer';
 
 const AboutUs = () => {
 
-    return (
-        // <div className="w-full h-screen flex justify-center items-center">
-        //     <span className="font-bold font-bit text-9xl">HELLO I AM 1!</span>
-        //     <button className="btn btn-primary">Play</button>
-        // </div>
-        <div>
-        <Drawer></Drawer>
-        
+    
+  const [showContent, setShowContent] = useState(false);
+
+  // Function to handle the play button click
+  const handlePlayClick = () => {
+    setShowContent(true);
+  };
+
+  return (
+    <div>
+      <Drawer />
+
+      {showContent ? (
+        // Display the content after pressing the button
         <div className="relative h-screen flex flex-col justify-center items-center">
-        <div
-          id="background"
-          className="absolute inset-0 z-0"
-          style={{
-            backgroundImage: `url(/aboutus-bg1.svg)`,
-            backgroundSize: "cover",
-          }}
-        ></div>
+          <div
+            id="background"
+            className="absolute inset-0 z-0"
+            style={{
+              backgroundImage: `url(/aboutus-bg1.svg)`,
+              backgroundSize: "cover",
+            }}
+          ></div>
 
-    <div className="relative z-10 p-8 text-center">
-        <span className="font-bold font-bit text-4xl sm:text-6xl lg:text-9xl">
-          About Us
-        </span>
+          <div className="relative z-10 p-8 text-center">
 
-        <button className="btn btn-primary mt-4">
-          Play
-        </button>
-      </div>
+            {/* Add your content here */}
+          </div>
+        </div>
+      ) : (
+        // Display the play button initially
+        <div className="w-full h-screen flex justify-center items-center">
+          <div
+            id="background"
+            className="absolute inset-0 z-0"
+            style={{
+              backgroundImage: `url(/aboutus-bg1.svg)`,
+              backgroundSize: "cover",
+            }}
+          ></div>  
+          <div className="relative z-10 p-8 text-center">
+            <span className="font-bold font-bit text-4xl sm:text-6xl lg:text-9xl">
+              About Us
+            </span>
+          <button className="btn btn-primary" onClick={handlePlayClick}>
+            Play
+          </button>
+        </div>
+        </div>
+      )}
+    </div>
+  );
+};
 
-
-      </div>
-      </div>
-    )
-}
-
-export default AboutUs
+export default AboutUs;
