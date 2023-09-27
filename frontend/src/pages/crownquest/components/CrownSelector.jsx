26e59bafad792eebbcff209e379541906b40d3cf
Joshua Corpuz <104405536+ZyWick@users.noreply.github.com>
2023-09-27 21:56:01 +0800

    added magnetic carousel with box placeholders
    TODO: remove scroll bar
    test if touch scroll work on mobile

--- frontend/src/pages/crownquest/components/CrownSelector.jsx
+++ frontend/src/pages/crownquest/components/CrownSelector.jsx
@@ -2,19 +2,44 @@ import React from "react";
 
 const CrownSelector = ({ elites, handleSelect }) => {
   return (
-    <div className="flex gap-10">
-      {elites?.map((elite) => (
-        <div
-          className="avatar border-2 rounded-md border-primary"
-          onClick={(e) => handleSelect(e, elite)}
-        >
-          <div className="w-44 rounded">
-            <img src={elite.image} />
-          </div>
-        </div>
-      ))}
+    <div className="gap-12">
+    <div className="flex max-w-full gap-8 py-3 overflow-x-auto snap-x snap-mandatory scroll-smooth touch-pan-x before:shrink-0 before:w-[3vw] after:shrink-0 after:w-[3vw]">
+      <div className="avatar border-2 rounded-md border-primary shrink-0 snap-center hover:scale-105" >
+      <div className="w-44 rounded"></div></div>
+      <div className="avatar border-2 rounded-md border-primary shrink-0 snap-center hover:scale-105" >
+      <div className="w-44 rounded"></div></div>
+      <div className="avatar border-2 rounded-md border-primary shrink-0 snap-center hover:scale-105" >
+      <div className="w-44 rounded"></div></div>
+      <div className="avatar border-2 rounded-md border-primary shrink-0 snap-center hover:scale-105" >
+      <div className="w-44 rounded"></div></div>
+      <div className="avatar border-2 rounded-md border-primary shrink-0 snap-center hover:scale-105">
+      <div className="w-44 rounded"></div></div>
+      <div className="avatar border-2 rounded-md border-primary shrink-0 snap-center hover:scale-105" >
+      <div className="w-44 rounded"></div></div>
+      <div className="avatar border-2 rounded-md border-primary shrink-0 snap-center hover:scale-105" >
+      <div className="w-44 rounded"></div></div>
+      <div className="avatar border-2 rounded-md border-primary shrink-0 snap-center hover:scale-105" >
+      <div className="w-44 rounded"></div></div>
+      <div className="avatar border-2 rounded-md border-primary shrink-0 snap-center hover:scale-105" >
+      <div className="w-44 rounded"></div></div>
+      <div className="avatar border-2 rounded-md border-primary shrink-0 snap-center hover:scale-105" >
+      <div className="w-44 rounded"></div></div>
+      <div className="avatar border-2 rounded-md border-primary shrink-0 snap-center hover:scale-105" >
+      <div className="w-44 rounded"></div></div>
+    </div>
     </div>
   );
 };
 
 export default CrownSelector;
+
+// {elites?.map((elite) => (
+//   <div
+//     className="avatar border-2 rounded-md border-primary shrink-0 snap-center hover:scale-105"
+//     onClick={(e) => handleSelect(e, elite)}
+//   >
+//     <div className="w-44 rounded">
+//       <img src={elite.image} />
+//     </div>
+//   </div>
+// ))}
