4d32682b2a4c39e11c1820b026dcfda5bcb6f58a
Nathan Andres <nathandres2001@gmail.com>
2023-09-28 13:41:55 +0800

    added: -finished crownquest -conditional display. chore: clean the ui a bit

--- frontend/src/components/error/Error.jsx
+++ frontend/src/components/error/Error.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Error = ({ error, textSize}) => {
     return (
-        <div className={`font-bit w-full h-full flex justify-center items-center drop-shadow-glow_red text-primary ${textSize}`}>
+        <div className={`font-bit w-full h-full flex justify-center items-center drop-shadow-glow_red text-white ${textSize}`}>
             {error}
         </div>
     )
--- frontend/src/components/loading/Loading.jsx
+++ frontend/src/components/loading/Loading.jsx
@@ -1,7 +1,7 @@
-const Loading = () => {
+const Loading = ({ color }) => {
     return (
         <div  className="absolute flex justify-center items-center w-full h-full drop-shadow-glow_green">
-            <div className="loading loading-bars w-1/6 text-emerald-400"></div>
+            <div className={`loading loading-bars w-1/6 ${color ? color : 'text-emerald-400'}`}></div>
         </div>
     )
 }
