d514ff63ac60baa0941903498c0e9c8adbe020de
Rich Pecson <richard_pecson@dlsu.edu.ph>
2023-09-29 16:45:02 +0800

    fixed: -fixed change color bug

--- frontend/src/pages/faqs/components/Question.jsx
+++ frontend/src/pages/faqs/components/Question.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Answer from './Answer'
 
 const Question = ({QA}) => {
+  const [isClicked, setIsClicked] = useState(false);
+
+  const toggleClicked = () => {
+    setIsClicked(!isClicked);
+    console.log('i have been clicked')
+  };
+
   return (
     <div className="collapse collapse-plus join-item">
-        <input type="checkbox" /* uncomment for radio : name = "test" */ /> 
-        <div className="collapse-title text-md font-medium bg-primary">
+        <input type="checkbox" /* uncomment for radio : name = "test" */ onClick={toggleClicked} />
+        <div className= {`bg-${isClicked ? 'primary' : 'neutral'} collapse-title text-md font-medium rounded-none`} >
             {QA.question}
         </div>
         <div className="collapse-content bg-white rounded-none"> 
