7895c75c5d32deda3494e13068c4b948ced98a38
Kovie <thekovie@gmail.com>
2023-09-29 17:56:59 +0800

    fix: z-index priority and page responsiveness

--- frontend/src/pages/individual_org/components/TopPageOrg.jsx
+++ frontend/src/pages/individual_org/components/TopPageOrg.jsx
@@ -3,18 +3,18 @@ const TopPageOrg = () => {
     return (
         <>
         
-        <div className="top-page-org bg-indigo-500 w-full flex flex-col mt-20 justify-center items-center shadow-2xl">
-            <section className="gallery w-1/2 py-0.5 lg:py-7">
-                <img src="https://scontent.fmnl17-5.fna.fbcdn.net/v/t39.30808-6/334090620_881525203057263_2025812858826874813_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=52f669&_nc_eui2=AeHTAefJWJf6LsHCjb38UcAInIdZDS_D1MOch1kNL8PUw1Kqmlz-jokNpWkQU5qtSc02yi0lz8Lr7twBEhYygADt&_nc_ohc=b_BD66CLzSIAX91Pzur&_nc_ht=scontent.fmnl17-5.fna&oh=00_AfB0kzkyd3WuSIZgfxpKKPiAMT6hcRZ4sk1XYIkqF5_D4g&oe=651B2C34" className="rounded-lg"></img>
+        <div className="top-page-org -z-10 bg-indigo-500 w-full flex flex-col pt-24 pb-20 px-4 md:px-8 lg:px-48 xl:px-80 justify-center items-center shadow-2xl">
+            <section className="gallery py-0.5 lg:py-7">
+                <img src="https://scontent.fmnl17-5.fna.fbcdn.net/v/t39.30808-6/334090620_881525203057263_2025812858826874813_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=52f669&_nc_eui2=AeHTAefJWJf6LsHCjb38UcAInIdZDS_D1MOch1kNL8PUw1Kqmlz-jokNpWkQU5qtSc02yi0lz8Lr7twBEhYygADt&_nc_ohc=b_BD66CLzSIAX91Pzur&_nc_ht=scontent.fmnl17-5.fna&oh=00_AfB0kzkyd3WuSIZgfxpKKPiAMT6hcRZ4sk1XYIkqF5_D4g&oe=651B2C34" className="rounded-lg" alt="gallery" />
             </section>
-            <span className="text-3xl lg:text-4xl xl:text-5xl font-sans font-bold text-black">
+            <span className="text-3xl lg:text-4xl xl:text-5xl font-sans font-bold text-black mt-8">
                 CSO
             </span>
             <span className="text-lg lg:text-xl xl:text-2xl font-sans font-normal text-black">
                 Always for service
             </span>
             <section className="org-logo flex justify-center items-center lg:w-[80%] py-3 lg:py-7" >
-                <img src="https://scontent.fmnl17-2.fna.fbcdn.net/v/t39.30808-6/327588310_854047155698952_8763889778503930565_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=a2f6c7&_nc_eui2=AeHwvIyol0xidSvfHjmm63zmNNuTaXhFtS8025NpeEW1L6FeFgmzPj7J3WZZRzCZmJP8sP2xspboyN4J406OtOuv&_nc_ohc=74fQ0aH87MIAX_W3b0t&_nc_ht=scontent.fmnl17-2.fna&oh=00_AfCki5Q-1EntllNOlOZYVvVsnjwILF7Fl1BOCuO_v6Pndw&oe=651BB043" alt="org-logo" className="w-1/4 object-contain rounded-full"></img>
+                <img src="https://scontent.fmnl17-2.fna.fbcdn.net/v/t39.30808-6/327588310_854047155698952_8763889778503930565_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=a2f6c7&_nc_eui2=AeHwvIyol0xidSvfHjmm63zmNNuTaXhFtS8025NpeEW1L6FeFgmzPj7J3WZZRzCZmJP8sP2xspboyN4J406OtOuv&_nc_ohc=74fQ0aH87MIAX_W3b0t&_nc_ht=scontent.fmnl17-2.fna&oh=00_AfCki5Q-1EntllNOlOZYVvVsnjwILF7Fl1BOCuO_v6Pndw&oe=651BB043" alt="org-logo" className="w-1/4 object-contain rounded-full" />
             </section>
         </div>
     </>
