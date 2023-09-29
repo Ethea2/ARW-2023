8625efb17e4475cb9f1224f0e5d342011f04db90
BanzaiNamco <orrin_landon_uy@dlsu.edu.ph>
2023-09-29 16:03:46 +0800

    actual accordion v1

--- frontend/src/pages/faqs/FAQs.jsx
+++ frontend/src/pages/faqs/FAQs.jsx
@@ -3,12 +3,21 @@ import Question from './components/Question'
 
 const FAQs = () => {
 
-  const QA = [{"question" : "What is annual recruitment week?", "answer" : "IDK bro"}]
+  const QA = [
+    {"question" : "What is annual recruitment week?", 
+    "answer" : "IDK bro"},
+    {"question" : "How do I join?",
+    "answer" : "asdasdasdas asd akjhdas kljhaskdljahsd aaksdhask jdhaskj dhaskjdh akjsdhak jshdkj"},
+    {"question" : "How do I join?",
+    "answer" : "click on buttons"},
+    {"question" : "How do I join?",
+    "answer" : "click on buttons"}
+  ]
 
   return (
     <div className="flex flex-col items-center justify-center">
         <h1 className="mt-faq-header mb-4 text-7xl font-bit">FAQs</h1>
-        <div className = "px-8">
+        <div className = "px-8 min-w-full join join-vertical">
           {
             QA.map((q, i) => (
               <Question QA={q} key ={i}/>
--- frontend/src/pages/faqs/components/Question.jsx
+++ frontend/src/pages/faqs/components/Question.jsx
@@ -3,12 +3,12 @@ import Answer from './Answer'
 
 const Question = ({QA}) => {
   return (
-    <div className="collapse collapse-plus">
-        <input type="checkbox" /> 
+    <div className="collapse collapse-plus join-item">
+        <input type="checkbox" /* uncomment for radio : name = "test" */ /> 
         <div className="collapse-title text-md font-medium bg-primary">
             {QA.question}
         </div>
-        <div className="collapse-content bg-white"> 
+        <div className="collapse-content bg-white rounded-none"> 
             <Answer answer = {QA.answer}/>
         </div>
     </div>
