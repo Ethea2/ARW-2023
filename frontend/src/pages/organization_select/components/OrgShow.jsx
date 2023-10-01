13b1930059fc20f16725f9de2f4273b14ec2c548
cpascasio <christian_james_pascasio@dlsu.edu.ph>
2023-10-02 05:04:57 +0800

    added: styles to Org Show chore: will fix the styling ASAP

--- frontend/src/pages/organization_select/components/OrgShow.jsx
+++ frontend/src/pages/organization_select/components/OrgShow.jsx
@@ -16,7 +16,7 @@ const OrgShow = ({ org, error }) => {
 
     const handleClick = () => {
         nav("/" + org?._id);
-    };  
+    };
     if (error) {
         return <Error error={error} />;
     }
@@ -26,12 +26,49 @@ const OrgShow = ({ org, error }) => {
             {noOrg ? (
                 <div>No Org Selected</div>
             ) : (
-                <div
-                    onClick={handleClick}
-                    className="flex flex-col gap-5 justify-center items-center border-2 cursor-pointer"
-                >
-                    <p>ORGNAME: {org.comp_name}</p>
-                    <p>DESCRIPTION: {org.desc}</p>
+                <div className="mt-10 p-5 border border-black">
+                    <div
+                        onClick={handleClick}
+                        className="flex flex-col gap-5 mt-10 bg-[#C6C6C6] justify-center items-center border cursor-pointer border-black rounded-md w-[100%] h-[100%]"
+                    >
+                        <div className="bg-[#B60000] p-2 rounded-t-md justify-between">
+                            <div className=" flex items-start px-2 w-[80%] md:w-[40%]">
+                                <p
+                                    className="w-full rounded-t-md w-full py-1 text-white" // Changed text color to black
+                                >
+                                    ENGAGE
+                                </p>
+                            </div>
+                        </div>
+
+                        {console.log(org)}
+                        <div className="w-64 carousel rounded-box">
+                            <div className="carousel-item w-full">
+                                <img
+                                    src={org.logoURL}
+                                    className="w-full"
+                                    alt=""
+                                />
+                            </div>
+                        </div>
+
+                        <div className="flex flex-col gap-5 border border-black ">
+                            <h1 className="text-2xl">{org.abbrv_name}</h1>
+                        </div>
+
+                        <div className="flex flex-col gap-5 border border-black ">
+                            <p className="text-sm">{org.comp_name}</p>
+                        </div>
+
+                        <div className="flex flex-col border border-black ">
+                            <p className="px-5 text-xs">{org.desc}</p>
+                        </div>
+
+
+                        <button className="btn btn-xs rounded-full sm:btn-sm md:btn-md lg:btn-lg mb-5 bg-[#B60000]">LEARN MORE</button>
+
+
+                    </div>
                 </div>
             )}
         </div>
