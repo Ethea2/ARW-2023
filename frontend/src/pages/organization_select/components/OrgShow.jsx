b34e572ea56d6db67c4aa7c492abb4759c75bf87
cpascasio <christian_james_pascasio@dlsu.edu.ph>
2023-10-02 18:51:58 +0800

    added: fixed the styling of OrgShow

--- frontend/src/pages/organization_select/components/OrgShow.jsx
+++ frontend/src/pages/organization_select/components/OrgShow.jsx
@@ -26,32 +26,39 @@ const OrgShow = ({ org, error }) => {
             {noOrg ? (
                 <div>No Org Selected</div>
             ) : (
-                <div className="mt-10 p-5 border border-black">
-                    <div
-                        className="flex flex-col gap-5 mt-10 bg-[#C6C6C6] justify-center items-center border border-black rounded-md w-[100%] h-[100%]"
-                    >
-                        <div className="bg-[#B60000] p-2 rounded-t-md justify-between">
-                            <div className=" flex items-start px-2 w-[80%] md:w-[40%]">
-                                <p
-                                    className="w-full rounded-t-md py-1 text-white" // Changed text color to black
-                                >
-                                    ENGAGE
-                                </p>
-                            </div>
-                        </div>
+                <div className="mt-10 p-5 border justify-center items-center  border-black">
 
-                        {console.log(org)}
+
+
+<div className="p-2 h-full w-full flex flex-col items-center">
+            <div className="w-[80%] mb-6">
+                <div className="bg-[#B60000] p-2 rounded-t-md flex justify-between">
+                    <div>
+                        <p
+                        className="w-full rounded-t-md w-full py-1 text-white" // Changed text color to black
+                        >
+                            ENGAGE
+                        </p>
+                    </div>
+                </div>
+
+                <div className="flex flex-wrap bg-[#C6C6C6] rounded-b-md md:flex-nowrap">
+
+                    <div>
                         <div className="w-64 carousel rounded-box">
                             <div className="carousel-item w-full">
                                 <img
                                     src={org.logoURL}
-                                    className="w-full"
+                                    className="w-full p-5 "
                                     alt=""
                                 />
                             </div>
                         </div>
+                    </div>
+                        
+                        <div className="gap-5 p-5">
 
-                        <div className="flex flex-col gap-5 border border-black ">
+                        <div className="flex flex-col border border-black ">
                             <h1 className="text-2xl">{org.abbrv_name}</h1>
                         </div>
 
@@ -64,10 +71,24 @@ const OrgShow = ({ org, error }) => {
                         </div>
 
 
-                        <button className="btn btn-xs rounded-full sm:btn-sm md:btn-md lg:btn-lg mb-5 bg-[#B60000]" onClick={handleClick}>LEARN MORE</button>
+                        <button className="btn btn-xs rounded-full sm:btn-sm md:btn-md lg:btn-lg mb-5 bg-[#B60000]">LEARN MORE</button>
 
 
-                    </div>
+                        
+                        </div>
+
+
+                        
+
+
+
+
+                </div>
+
+            </div>
+
+
+        </div>                    
                 </div>
             )}
         </div>
