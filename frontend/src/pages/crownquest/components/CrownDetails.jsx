a2ab5c6cdc9c309915a5017c2e796afb95c9ce7b
Nathan Andres <nathandres2001@gmail.com>
2023-10-02 21:46:20 +0800

    added: fixed responsive animation

--- frontend/src/pages/crownquest/components/CrownDetails.jsx
+++ frontend/src/pages/crownquest/components/CrownDetails.jsx
@@ -8,14 +8,14 @@ const CrownDetails = ({ activeElite }) => {
         visible: { opacity: 1, x: 0, transition: { duration: 0.5 } },
     };
     return (
-        <div className="w-full h-1/2 lg:w-1/2 lg:h-full flex m-auto items-center">
+        <div className="w-full min-h-1/3 lg:w-1/2 lg:h-full flex m-auto items-center bg-black/50 lg:bg-inherit">
             {activeElite ? (
                 <motion.p
                     initial="hidden"
                     animate={activeElite ? "visible" : "hidden"}
                     variants={textVariants}
                     key={activeElite ? activeElite.desc : ""}
-                    className="text-white lg:text-black text-xs lg:text-lg text-justify bg-black/50 p-4 lg:bg-inherit"
+                    className="text-white lg:text-black text-xs lg:text-lg text-justify p-4"
                 >
                     {activeElite.desc}
                 </motion.p>
