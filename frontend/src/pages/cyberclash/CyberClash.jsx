6c104d8a5886e07e98074a0a596f781bc73b9995
miiiigs <97244382+miiiigs@users.noreply.github.com>
2023-10-02 18:24:32 +0800

    text shadow/outline

--- frontend/src/pages/cyberclash/CyberClash.jsx
+++ frontend/src/pages/cyberclash/CyberClash.jsx
@@ -9,34 +9,36 @@ const CyberClash = () => {
     const { data: cyberClash, loading, error } = useFetch("/api/clash");
 
     return (
-        <div className="flex justify-center items-center flex-col w-full h-screen ">
-            <div
-                id="container"
-                className="flex flex-col items-center border-8 rounded-xl border-base-100 w-10/12 h-4/5 mt-20"
-            >
-                <div
-                    id="title"
-                    className="relative -top-5 text-3xl text-[#FFD524] font-normal font-header text-header-shadow"
-                >
-                    LEADERBOARDS
-                </div>
-
-                <ChampionContatiner />
-
-                {loading ? (
+        <div className="flex justify-center items-center flex-col w-full h-full pb-10 ">
+            
+            {loading ? (
                     <Loading color={"text-slate-200"} />
                 ) : error ? (
                     <Error error={error} />
                 ) : (
                     <>
-                        <div className="h-full w-full flex flex-col md:grid md:grid-flow-col md:grid-cols-3 justify-start items-center">
-                            {cyberClash?.map((game) => (
-                                <GameContainer game={game} />
-                            ))}
+                    <div
+                        id="container"
+                        className="flex flex-col items-center border-8 rounded-xl border-base-100 w-10/12 h-4/5 mt-20 pb-10"
+                    >
+                        <div
+                            id="title"
+                            className="relative -top-5 text-3xl text-[#FFD524] text-outline font-normal font-header  md:text-7xl md:-top-10"
+                        >
+                            LEADERBOARDS
                         </div>
+
+                        <ChampionContatiner />
+
+                                <div className="h-full w-full flex flex-col md:w-11/12 md:grid md:grid-flow-col md:grid-cols-3 justify-start items-center">
+                                    {cyberClash?.map((game) => (
+                                        <GameContainer game={game} />
+                                    ))}
+                                </div>
+                            
+                    </div>
                     </>
-                )}
-            </div>
+            )}
         </div>
     );
 };
--- frontend/src/pages/cyberclash/components/ChampionContainer.jsx
+++ frontend/src/pages/cyberclash/components/ChampionContainer.jsx
@@ -2,9 +2,11 @@ import React from "react";
 
 const ChampionContatiner = ({ cyberClash }) => {
     return (
-        <div className="flex justify-center flex-row items-center border-2 border-orange-500 rounded-xl w-11/12 dark-bg p-3">
-            <div className="grid content-center w-1/4 aspect-square  mr-2.5">
-                <img src="../crown.png" alt="CrownImg" className="" />
+        
+        <div className = 'flex justify-center flex-row items-center border-2 border-orange-500 rounded-xl w-11/12 dark-bg p-3 mt-6 md:mt-20'>
+            
+            <div  className="grid content-center w-1/4 aspect-square  mr-2.5">
+                <img src="../crown.png" alt="CrownImg" className="rounded-full w-full"/>
             </div>
 
             <div className="flex justify-center flex-col items-center w-3/4 ">
