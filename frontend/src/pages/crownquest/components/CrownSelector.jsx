7d972a1129119a83328422e099340d56081a7ab6
Nathan Andres <nathandres2001@gmail.com>
2023-10-02 12:24:24 +0800

    added: animations for crown select

--- frontend/src/pages/crownquest/components/CrownSelector.jsx
+++ frontend/src/pages/crownquest/components/CrownSelector.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import Crown from "../../../assets/Crown";
+import Crown from "../../../assets/crownquest/crown.png";
+import { motion } from "framer-motion";
 
 const CrownSelector = ({ elites, handleSelect, selectedElite }) => {
     return (
-        <div className="w-full h-full flex items-center gap-2 overflow-x-auto snap-x snap-mandatory scroll-smooth touch-pan-x before:shrink-0 after:shrink-0 after:w-[3vw]">
+        <div className="w-full h-full flex items-center gap-6 overflow-x-auto snap-x snap-mandatory scroll-smooth touch-pan-x before:shrink-0 after:shrink-0 after:w-[3vw]">
             {elites?.map((elite) => (
                 <div
                     onClick={(e) => handleSelect(e, elite)}
@@ -14,7 +15,17 @@ const CrownSelector = ({ elites, handleSelect, selectedElite }) => {
                     }`}
                 >
                     <div className="flex">
-                        <Crown />
+                        {selectedElite?.orgname === elite.orgname && (
+                            <motion.img
+                                src={Crown}
+                                className="absolute bottom-12 left-10"
+                                initial={{ opacity: 0, scale: 1, rotate: 30  }}
+                                animate={{ opacity: 1, scale: 0.5 }}
+                                exit={{ opacity: 0, scale: 1 }}
+                                transition={{ ease: "easeOut", duration: 0.3 }}
+                            />
+                        )}
+
                         <img
                             src={elite.orglogo}
                             className={`border-2  rounded-xl bg-black/50 hover:border-[#FFD524] transition ease-in ease-out ${
