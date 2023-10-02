95212193d347ef293615cf5fcc0f03e7a0f979e4
Nathan Andres <nathandres2001@gmail.com>
2023-10-02 17:59:06 +0800

    added: -styled components a little bit

--- frontend/src/pages/faqs/components/Tabs.jsx
+++ frontend/src/pages/faqs/components/Tabs.jsx
@@ -4,29 +4,29 @@ import React, { useState } from "react";
 import { TABS } from "../constants/queries";
 const Tabs = ({ selected, setSelected }) => {
     return (
-        <div className="relative z-10 flex flex-wrap items-center justify-center gap-4">
+        <div className="w-full relative z-10 flex flex-wrap items-center justify-center gap-4">
             {TABS.map((tab) => (
                 <button
                     onClick={() => setSelected(tab)}
-                    className={`relative overflow-hidden whitespace-nowrap rounded-md border-[1px] px-3 py-1.5 text-sm font-medium transition-colors duration-500 ${
+                    className={`relative overflow-hidden whitespace-nowrap rounded-md px-3 py-1.5 text-sm font-medium transition-colors bg-black/10 duration-500 ${
                         selected === tab
-                            ? "border-violet-500 text-slate-50"
-                            : "border-slate-600 bg-transparent text-slate-400"
+                            ? " text-slate-50"
+                            : ""
                     }`}
                     key={tab}
                 >
-                    <span className="relative z-10">{tab}</span>
+                    <span className="relative z-10 text-black text-md font-semibold">{tab}</span>
                     <AnimatePresence>
                         {selected === tab && (
                             <motion.span
-                                initial={{ y: "100%" }}
-                                animate={{ y: "0%" }}
-                                exit={{ y: "100%" }}
+                                initial={{ x: "-100%" }}
+                                animate={{ x: "0%" }}
+                                exit={{ x: "100%" }}
                                 transition={{
                                     duration: 0.5,
                                     ease: "backIn",
                                 }}
-                                className="absolute inset-0 z-0 bg-gradient-to-r from-violet-600 to-indigo-600"
+                                className="absolute inset-0 z-0 bg-[#FFD524]"
                             />
                         )}
                     </AnimatePresence>
