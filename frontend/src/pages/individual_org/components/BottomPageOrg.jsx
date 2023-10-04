732de82792f8d33ee69b8cca566b14eb7c0853e6
Paula Pacheco <100471162+ennxxx@users.noreply.github.com>
2023-10-04 18:50:08 +0800

    added fb link icon

--- frontend/src/pages/individual_org/components/BottomPageOrg.jsx
+++ frontend/src/pages/individual_org/components/BottomPageOrg.jsx
@@ -18,21 +18,28 @@ const BottomPageOrg = ({ org }) => {
                                 </span>
                             </section>
                             <section className="org-desc text-justify mt-5">
-                                <span className="text-md xl:text-lg font-monda font-normal  text-[#242640]">
+                                <span className="text-md xl:text-lg font-monda font-normal text-[#242640]">
                                     {org.desc}
                                 </span>
                             </section>
-                            <section className="org-facebook-link flex flex-col justify-center pt-5">
-                                <span className="text-lg xl:text-xl wrap break-words lg:break-normal font-monda text-red-500 font-bold hover:underline hover:text-red-500 text-center xl:text-left">
-                                    <a
-                                        href={org.fb_link}
-                                        className="w-full"
-                                        target="_blank"
-                                        rel="noreferrer"
+                            <section className="org-facebook-link flex flex-col ml-auto justify-center pt-5 xl:pt-10">
+                                <a
+                                    href={org.fb_link}
+                                    className="w-full"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        className="h-6 w-6"
+                                        fill="#F83F65"
+                                        viewBox="0 0 24 24"
                                     >
-                                        {org.fb_link}
-                                    </a>
-                                </span>
+                                        <path
+                                            d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"
+                                        />
+                                    </svg>
+                                </a>
                             </section>
                         </div>
                     </div>
