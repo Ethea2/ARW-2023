4d20fbfa1fd91dc02a86e77a9854b423ef8d3909
JefJefHoorraayy <88278313+JefJefHoorraayy@users.noreply.github.com>
2023-10-04 22:32:51 +0800

    added: changes to element margins, carousel images to assets, button asset. todo: add carousel for exe comm, replace current play button with provided asset, make invaders.svg responsive to screen size

--- frontend/src/pages/aboutus/components/ARWDesciption.jsx
+++ frontend/src/pages/aboutus/components/ARWDesciption.jsx
@@ -4,39 +4,43 @@ import containerTop from "../../../assets/aboutus/bit-container-top.svg";
 import containerBottom from "../../../assets/aboutus/bit-container-bottom.svg";
 
 const arwDesciption = () => {
-  return (
-    <div>
-      <span className="font-bold font-bit text-4xl sm:text-6xl lg:text-9xl text-primary">ABOUT US</span>
+    return (
+        <div>
+            <span className="font-bold font-bit text-4xl sm:text-6xl lg:text-9xl text-primary">
+                ABOUT US
+            </span>
 
-      {/* Top portion of container */}
-      <img className="w-screen mt-10" src={containerTop} alt="bit-top" />
+            {/* Top portion of container */}
+            <img className="w-screen mt-10" src={containerTop} alt="bit-top" />
 
-      {/* Middle portion of container / Content */}
-      <div
-        className="bg-no-repeat bg-cover px-8 -my-1"
-        style={{
-          backgroundImage: `url(${containerMid})`,
-        }}
-      >
-        <div className="p-5 border-8 rounded-3xl border-red-900 border-solid bg-white">
-          <span className="font-bit">
-            ANNUAL RECRUITMENT WEEK <br></br>
-          </span>
-          <span className="text-black">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </span>
+            {/* Middle portion of container / Content */}
+            <div
+                className="bg-no-repeat bg-cover px-8 -my-1"
+                style={{
+                    backgroundImage: `url(${containerMid})`,
+                }}
+            >
+                <div className="p-5 border-8 rounded-3xl border-red-900 border-solid bg-white md:mx-12">
+                    <span className="font-bit">
+                        ANNUAL RECRUITMENT WEEK <br></br>
+                    </span>
+                    <span className="text-black">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore
+                        magna aliqua. Ut enim ad minim veniam, quis nostrud
+                        exercitation ullamco laboris nisi ut aliquip ex ea
+                        commodo consequat. Duis aute irure dolor in
+                        reprehenderit in voluptate velit esse cillum dolore eu
+                        fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                        non proident, sunt in culpa qui officia deserunt mollit
+                        anim id est laborum.
+                    </span>
+                </div>
+            </div>
+            {/* Bottom portion of container */}
+            <img className="w-screen" src={containerBottom} alt="bit-bottom" />
         </div>
-      </div>
-      {/* Bottom portion of container */}
-      <img className="w-screen" src={containerBottom} alt="bit-bottom" />
-    </div>
-  );
+    );
 };
 
 export default arwDesciption;
