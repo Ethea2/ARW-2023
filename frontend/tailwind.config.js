ec940d051c24c9dd0e6cbb565e0c28538b653f0e
Nathan Andres <nathandres2001@gmail.com>
2023-10-05 22:11:06 +0800

    fixed carousel

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,53 +2,54 @@
 module.exports = {
     content: [
         "./src/**/*.{js,jsx,ts,tsx}",
+        "node_modules/daisyui/dist/**/*.js",
+        "node_modules/react-daisyui/dist/**/*.js",
     ],
     theme: {
         extend: {
             fontFamily: {
-                'bit': ['"Press Start 2P"', 'cursive'],
-                'monda':['"Monda"', '"sans-serif"'],
-                'header': ['"Bungee"', 'cursive']
+                bit: ['"Press Start 2P"', "cursive"],
+                monda: ['"Monda"', '"sans-serif"'],
+                header: ['"Bungee"', "cursive"],
             },
             dropShadow: {
                 glow_red: [
                     "0 0px 20px rgba(239, 68, 68, 0.35)",
-                    "0 0px 65px rgba(252, 165, 165, 0.2)"
+                    "0 0px 65px rgba(252, 165, 165, 0.2)",
                 ],
                 glow_green: [
                     "0 0px 20px rgba(167, 243, 208, 0.35)",
-                    "0 0px 65px rgba(52, 211, 153, 0.2)"
-                ]
+                    "0 0px 65px rgba(52, 211, 153, 0.2)",
+                ],
             },
             margin: {
-                'faq-header': '120px',
+                "faq-header": "120px",
             },
         },
         borderRadius: {
-            'none': '0',
-            'sm': '0.125rem',
-            DEFAULT: '0.25rem',
-            'md': '0.375rem',
-            'lg': '0.5rem',
-            'full': '9999px',
-            'large': '12px',
-            'faq-ans-custom': '44px',
+            none: "0",
+            sm: "0.125rem",
+            DEFAULT: "0.25rem",
+            md: "0.375rem",
+            lg: "0.5rem",
+            full: "9999px",
+            large: "12px",
+            "faq-ans-custom": "44px",
         },
-        
     },
     daisyui: {
         themes: [
             {
-                'arw-theme': {
-                    'primary': '#FF2955',
-                    'secondary': '#FFD524',
-                    'accent': '#00F6FF',
-                    'neutral': '#F87DA4',
-                    'base-100': '#451F4D',
-                }
-            }
+                "arw-theme": {
+                    primary: "#FF2955",
+                    secondary: "#FFD524",
+                    accent: "#00F6FF",
+                    neutral: "#F87DA4",
+                    "base-100": "#451F4D",
+                },
+            },
         ],
-        base: false
+        base: false,
     },
     plugins: [require("daisyui")],
-}
+};
