c6b01fecc7f1243060ddbcf9c5811316c828ce05
Patty Arao <patricia_arao@dlsu.edu.ph>
2023-10-06 19:37:57 +0800

    added: responsive design

--- frontend/src/pages/cyberclash/components/TeamContainer.jsx
+++ frontend/src/pages/cyberclash/components/TeamContainer.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export default function TeamContainer({ team, image }) {
     return (
-        <div className="w-[45%] h-full md:flex md:flex-col md:items-center bg-black/30 text-center rounded-lg text-white font-monda">
+        <div className="w-[45%] h-full md:flex md:flex-col md:items-center lg:w-[30%] bg-black/30 text-center rounded-lg text-white font-monda">
             <img src={image} className="bg-black/10 p-4 rounded-t-lg" />
             <Link to={`/${team}`} className="w-full cursor-pointer">
                 <h3 className="p-2 bg-[#F87DA4]/50 rounded-b-lg">{team}</h3>
