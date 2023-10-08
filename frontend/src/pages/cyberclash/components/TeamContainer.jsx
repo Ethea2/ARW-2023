ae988dea2639212c8a5e10b6539665730c285de8
Ethea2 <nathandres2001@gmail.com>
2023-10-09 00:31:13 +0800

    Added: full width for team name

--- frontend/src/pages/cyberclash/components/TeamContainer.jsx
+++ frontend/src/pages/cyberclash/components/TeamContainer.jsx
@@ -4,7 +4,7 @@ export default function TeamContainer({ team, image }) {
     return (
         <div className="w-[45%] h-full md:flex md:flex-col md:items-center lg:w-[30%] bg-black/30 text-center rounded-lg text-white font-monda">
             <img src={image} className="bg-black/10 p-4 rounded-t-lg" />
-            <h3 className="p-2 bg-[#F87DA4]/50 rounded-b-lg">{team}</h3>
+            <h3 className="p-2 bg-[#F87DA4]/50 rounded-b-lg w-full">{team}</h3>
         </div>
     );
 }
